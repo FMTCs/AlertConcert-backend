2b4f56a0f97395f5d20b841ba9409e6963eeb421
Kovalt03 <kalswp2003@naver.com>
2026-02-23 17:16:43 +0900

    [REFACTOR] 추천서비스 api error msg 리팩토링 동일 로직으로 처리하도록 수정
    ---
    Author: Kovalt03 <kalswp2003@naver.com>

--- src/main/java/fcmt/backend/exception/ErrorCode.java
+++ src/main/java/fcmt/backend/exception/ErrorCode.java
@@ -18,11 +18,13 @@ public enum ErrorCode {
 
 	AUTHENTICATION_FAILED(HttpStatus.UNAUTHORIZED, "AUTHENTICATION_FAILED", "아이디 혹은 비밀번호가 유효하지 않습니다"),
 	DUPLICATE_USER(HttpStatus.CONFLICT, "DUPLICATE_USER", "이미 존재하는 아이디입니다"),
-
+	USER_PREFERENCE_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_PREFERENCE_NOT_FOUND", "선호도 정보가 없습니다."),
 	// AI 사용 관련 에러 메시지
 	AI_API_ERROR(HttpStatus.BAD_GATEWAY, "A001", "AI 서비스와의 통신이 원활하지 않습니다."),
 	GENRE_NOT_FOUND(HttpStatus.PARTIAL_CONTENT, "A002", "가수 장르 정보를 분석할 수 없습니다."),
 
+	// spotify api 사용 관련 에러 메시지
+	SPOTIFY_API_ERROR(HttpStatus.BAD_GATEWAY, "SPOTIFY_API_ERROR", "외부 서비스(Spotify) 연동 중 오류가 발생했습니다."),
 	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "서버 내부 오류가 발생했습니다");
 
 	private final HttpStatus status;
